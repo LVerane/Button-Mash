a858e5f38ebe51a745a388d18d5843ff3679e986
Lucio Verane <verane64@gmail.com>
2019-10-08 18:55:51 -0500

    show/hide option for long text fixed in search reviews and added to random games info

--- ASSETS/JAVASCRIPT/review.js
+++ ASSETS/JAVASCRIPT/review.js
@@ -20,41 +20,23 @@ $(document).on('click', '#show-search', function () {
     getVideo();
 });
 
-console.log(window.location.search);
 var urlSearch = new URL(location.href).searchParams.get('navSearch');
 
 var topGames = ["portal-2", "red-dead-redemption-2", "dragon-age-origins-ultimate-edition", "the-legend-of-zelda-breath-of-the-wild", "heroes-of-might-and-magic-3-the-shadow-of-death", "marvels-spider-man", "divinity-original-sin-ii-definitive-edition", "warcraft-3-reign-of-chaos", "bioshock", "shin-megami-tensei-persona-4", "mortal-kombat-11"];
 var counter = 0;
 
-//
-var videoSearch = new URL(location.href).searchParams.get('navSearch');
-//
+var fullInfo;
+var countRandom;
+var fullReviews;
+var countSearch;
 
 if (urlSearch === null || urlSearch === "") {
     console.log("url is null")
     clearInfo();
     showRandomGame();
 } else {
-    //change it to reg rex
-    var game = urlSearch.split("");
-
-    for (var i = 0; i < game.length; i++) {
-        if (game[i] == " ") {
-            game[i] = "-"
-        }
-    }
-    urlSearch = game.join("");
-
-    //video
-    var video = videoSearch.split("");
-    //change it to reg rex
-    for (var i = 0; i < video.length; i++) {
-        if (video[i] == " ") {
-            video[i] = "%20"
-        }
-    }
-    videoSearch = video.join("");
-    //
+    urlSearch = urlSearch.replace(/ /gi, "-")
+    videoSearch = urlSearch.replace(/-/gi, "%20")
 
     clearRandom();
     getInfo();
@@ -85,12 +67,11 @@ function clearRandom() {
 
 function showRandomGame() {
     for (var i = 0; i < 6; i++) {
-        // var randomGame = Math.ceil(Math.random() * 358660);
-        // var randomLink = "https://api.rawg.io/api/games/" + randomGame;
-
         var randomPosition = Math.floor(Math.random() * topGames.length)
         var randomGame = topGames[randomPosition]
         var randomLink = "https://api.rawg.io/api/games/" + randomGame + "/suggested?page_size=10";
+        fullInfo = [];
+        countRandom = 0;
 
         $.ajax({
             url: randomLink,
@@ -105,7 +86,6 @@ function showRandomGame() {
                 url: randomSuggestionLink,
                 method: "GET",
             }).then(function (response) {
-                console.log(response)
 
                 website = $(`<a>`).attr({ "href": response.website, "target": "_blank" })
                 imageWeb = $(`<img>`).attr("src", response.background_image)
@@ -113,9 +93,19 @@ function showRandomGame() {
 
                 website.html(imageWeb);
 
+                var gameInfo = response.description;
+                if (gameInfo.length > 1100) {
+                    var shortInfo = gameInfo.substring(0, 1000);
+                    fullInfo.push(gameInfo)
+                    var span = $("<span class='showRandom' data-position='" + countRandom + "'>[Read More]</span>")
+                    $(`.random-game-${counter} .game-info`).html(shortInfo).attr("id", countRandom).append(span)
+                    countRandom++;
+                }else{
+                    $(`.random-game-${counter} .game-info`).html(gameInfo)
+                }
+
                 $(`.random-game-${counter} .about`).text(response.name)
                 $(`.random-game-${counter} .game-image`).html(website)
-                $(`.random-game-${counter} .game-info`).html(response.description)
                 counter++;
             });
         });
@@ -126,13 +116,10 @@ function showRandomGame() {
 //youtube api start
 function getVideo() {
     var videoURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + videoSearch + "%20trailer&key=AIzaSyBIJ6pbEXcv9in6Xi-Z8IogrfMpxKUXVy0"
-    console.log("videourl = " + videoURL)
     $.ajax({
         url: videoURL,
         method: "GET",
     }).then(function (response) {
-        console.log("response from videourl = " + response)
-        console.log("response items blah = " + response.items[0].id.videoId)
         var videoId = response.items[0].id.videoId;
         var videoLink = "https://www.youtube.com/watch?v=" + videoId;
         $(".trailer-link").attr("href", videoLink).text("Watch trailer");
@@ -152,14 +139,15 @@ function videoSetUp() {
 
 function getInfo() {
     var queryURL = "https://api.rawg.io/api/games/" + urlSearch;
-
+    
+    
     $.ajax({
         url: queryURL,
         method: "GET",
+        error: function() {
+            $("#about").text("That is not a game :(")
+        }
     }).then(function (response) {
-
-        console.log("got response from info");
-        console.log(response)
 
         platforms = $("<p>").text("Available for ")
         for (var i = 0; i < response.platforms.length; i++) {
@@ -177,7 +165,6 @@ function getInfo() {
         reddit = $(`<a>`).attr({ "href": response.reddit_url, "target": "_blank" })
         imageRed = $(`<img>`).attr("src", "ASSETS/IMAGES/redditLogoSmall.jpg")
         reddit.html(imageRed)
-
 
         metacritic = $(`<a>`).attr("href", response.metacritic_url).text(`Metacritic Score: ${response.metacritic}`)
 
@@ -201,64 +188,56 @@ function getInfo() {
 
 function getReview(oficialName) {
     var queryURL = "https://api.rawg.io/api/games/" + oficialName + "/reviews";
-    resetTruncate();
+    fullReviews = [];
+    countSearch = 0;
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-
+        
         $("#game-reviews").html("")
         $("#reviews").text("Reviews")
 
         for (var i = 0; i < response.results.length; i++) {
-
             review = response.results[i].text
-            review = review.replace(/<br\s*[\/]?>/gi, "</p><p>")
-
             newPRating = $(`<p>`).text(`Rating: ${response.results[i].rating}/5`)
-            newPReview = $(`<p>${review}</p>`).attr("class", "truncate").attr("id", count)
 
-            count++;
-
-            truncate();
+            var newPReview;
+            if (review.length > 600) {
+                var shortString = review.substring(0, 500);
+                fullReviews.push(review)
+                var span = $("<span class='show' data-position='" + countSearch + "'>[Read More]</span>")
+                newPReview = $(`<p>${shortString}</p>$`).attr("id", countSearch)
+                newPReview.append(span)
+                countSearch++;
+            }else{
+                newPReview = $(`<p>${review}</p>`)
+            }
 
             $("#game-reviews").append(newPReview, newPRating);
         }
     });
 }
 
-var fullReviews = [];
-var position = 0;
-var count = 0;
-
-function resetTruncate() {
-    fullReviews = [];
-    position = 0;
-    count = 0;
-}
-
-function truncate() {
-    var show = "<span class='show' data-show='" + position + "'>[Read More]</span>";
-
-    $(".truncate").each(function () {
-        var Element = this;
-        var FullText = $(Element).text();
-        var shortString = FullText.substring(0, 500);
-        if (FullText.length > 600) {
-            fullReviews.push(FullText)
-            $(Element).html(shortString + show);
-            position++;
-        }
-    });
-}
+//truncate show/hide
 
 $(document).on("click", ".show", function () {
-    position = $(this).attr("data-show")
-    $(`#${position}`).html(fullReviews[position] + "<span class='hide' data-hide='" + position + "'>[Read Less]</span>");
+    var position = $(this).attr("data-position")
+    $(`#${position}`).html(fullReviews[position] + "<span class='hide' data-position='" + position + "'>[Read Less]</span>");
 });
 
 $(document).on("click", ".hide", function () {
-    position = $(this).attr("data-hide")
-    $(`#${position}`).html(fullReviews[position].substring(0, 500) + "<span class='show' data-show='" + position + "'>[Read More]</span>");
+    var position = $(this).attr("data-position")
+    $(`#${position}`).html(fullReviews[position].substring(0, 500) + "<span class='show' data-position='" + position + "'>[Read More]</span>");
+});
+
+$(document).on("click", ".showRandom", function () {
+    var position = $(this).attr("data-position")
+    $(`#${position}`).html(fullInfo[position] + "<span class='hideRandom' data-position='" + position + "'>[Read Less]</span>");
+});
+
+$(document).on("click", ".hideRandom", function () {
+    var position = $(this).attr("data-position")
+    $(`#${position}`).html(fullInfo[position].substring(0, 1000) + "<span class='showRandom' data-position='" + position + "'>[Read More]</span>");
 });
